4eb475aaff19e66d1a3120b27e31d808b8ff8555
Jose Luis G <garciamartinezjoseluis@gmail.com>
2021-06-03 20:25:41 +0200

    fix github pages

--- sw.js
+++ sw.js
@@ -93,10 +93,10 @@ self.addEventListener('fetch', e => {
                     console.log('e', e.request);
                     //si es tipo html, muestro
                     if (e.request.headers.get('accept').includes('text/html')) {
-                        return caches.match('/pages/sin-conexion.html');
+                        return caches.match('pages/sin-conexion.html');
                         //si es tipo imagen, muestro
                     } else if (e.request.destination === 'image') {
-                        return caches.match('/img/no-image.png');
+                        return caches.match('img/no-image.png');
                     }
                 });
         });
